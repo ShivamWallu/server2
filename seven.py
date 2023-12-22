d89ea68a4bd4bde1982d94d8dee4ade935a54401
Shivam Wallu <141025670+ShivamWallu@users.noreply.github.com>
2023-12-22 16:08:51 +0530

    Update seven.py

--- seven.py
+++ seven.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = Flask(__name__)
 DB_USERNAME = 'stagedb'
 DB_PASSWORD = 'Stage@#$2023@'
-DB_HOST = 'http://db.urawarrior.com/'
+DB_HOST = 'db.urawarrior.com'
 DB_PORT = '3306'
 DB_NAME = 'stagedb'
 
